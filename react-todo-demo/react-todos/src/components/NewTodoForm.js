201b7839886022d26fb69175c6605dfb3456ef27
multisula <multisula@gmail.com>
2023-06-08 21:37:19 +0900

    Adding User Input to React Form

---
+++ react-todo-demo/react-todos/src/components/NewTodoForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function NewTodoForm() {
+  const [description, setDescription] = useState("");
+  const [assigned, setAssigned] = useState("");
+
+  return (
+    <div classname="mt-5">
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Assigned</label>
+          <input
+            type="text"
+            className="form-control"
+            required
+            onChange={(e) => setAssigned(e.target.value)}
+            value={assigned}
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Description</label>
+          <textarea
+            className="form-control"
+            rows={3}
+            required
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
+        </div>
+        <button type="button" className="btn btn-primary mt-3">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default NewTodoForm;
