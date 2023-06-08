a39544669d5cdb2ef25ca6731bf3f5d7040e1d29
multisula <multisula@gmail.com>
2023-06-08 22:10:44 +0900

    React conditional rendering

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -4,6 +4,8 @@ import TodoTable from "./components/TodoTable";
 import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
   const [todos, setTodos] = useState([
     { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One" },
     { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
@@ -45,10 +47,13 @@ function App() {
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
           <TodoTable todos={todos} deleteTodo={deleteTodo} />
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add new todo
+          <button
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+            className="btn btn-primary"
+          >
+            {showAddTodoForm ? "Close New Todo" : "New Todo"}
           </button>
-          <NewTodoForm addTodo={addTodo} />
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
         </div>
       </div>
     </div>
