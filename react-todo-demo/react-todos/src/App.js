e583c0775ec05b52dc976547a81d7be3b1f47d4f
multisula <multisula@gmail.com>
2023-06-08 21:17:42 +0900

    React onClick Listeners

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -12,12 +12,28 @@ function App() {
       rowAssigned: "User One",
     },
   ];
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  };
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
           <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
