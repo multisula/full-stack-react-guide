5efee8a7882fa1fec06d5230495ee2c144ef2fa6
multisula <multisula@gmail.com>
2023-06-08 18:04:04 +0900

    ts variables

--- typescript-training/02-variables/sample-types.js
+++ typescript-training/02-variables/sample-types.js
@@ -1,18 +1,15 @@
-let found: boolean = true;
-let grade: number = 88.6;
-let firstName: string = "Anup";
-let lastName: string = "Kumar";
-
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = "Kumar";
 /*
 // let's break it
 found = 0;
 grade = "A";
 firstName = false;
 */
-
 console.log(found);
 console.log("The grade is " + grade);
 console.log("Hi " + firstName + " " + lastName);
-
 // use template string
-console.log(`Hi ${firstName} ${lastName}`);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
