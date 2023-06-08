662da8497cf28a813c62469c3d8fb07306782f99
multisula <multisula@gmail.com>
2023-06-08 22:30:50 +0900

    type, todo model

--- react-todo-demo/react-todos/src/App.tsx
+++ react-todo-demo/react-todos/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     },
   ]);
 
-  const addTodo = (description, assigned) => {
+  const addTodo = (description: string, assigned: string) => {
     let rowNumber = 0;
 
     if (todos.length > 0) {
@@ -34,7 +34,7 @@ function App() {
     console.log(todos);
   };
 
-  const deleteTodo = (deleteTodoRowNumber) => {
+  const deleteTodo = (deleteTodoRowNumber: number) => {
     let filtered = todos.filter(function (value) {
       return value.rowNumber !== deleteTodoRowNumber;
     });
--- react-todo-demo/react-todos/src/components/TodoRowItem.tsx
+++ react-todo-demo/react-todos/src/components/TodoRowItem.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-function TodoRowItem(props) {
+function TodoRowItem(props: {
+  rowNumber: number;
+  rowDescription: string;
+  rowAssigned: string;
+  deleteTodo: Function;
+}) {
   return (
     <tr onClick={() => props.deleteTodo(props.rowNumber)}>
       <th scope="row">{props.rowNumber}</th>
