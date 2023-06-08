9f320f167cf5b9569e5fa30ca5c6df0cbf85a6ed
multisula <multisula@gmail.com>
2023-06-08 19:53:12 +0900

    interfaces

---
+++ typescript-training/07-interfaces/Driver.ts
@@ -0,0 +1,17 @@
+import { Coach } from "./Coach";
+import { CricketCoach } from "./CricketCoach";
+import { GolfCoach } from "./GolfCoach";
+
+let myCricketCoach = new CricketCoach();
+let myGolfCoach = new GolfCoach();
+
+// declare an array for coaches ... initially empty
+let theCoaches: Coach[] = [];
+
+// add the coaches to the array
+theCoaches.push(myCricketCoach);
+theCoaches.push(myGolfCoach);
+
+for (let coach of theCoaches) {
+  console.log(coach.getDailyWorkout());
+}
